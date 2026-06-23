c4ef033ecedf206ac6f2428f8ef0d1935b0000d6
Uma Karthik <umakarthik1309@gmail.com>
2026-06-23 14:59:40 +0530

    add task functionality

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from . import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    # TO DO
+    path('todo/',include('todo.urls')),
 ]
